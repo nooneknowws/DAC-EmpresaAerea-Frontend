d16d104e132e2e51dd185b9f0bbf217f5de52c0a
fumireko <fumireko@yandex.com>
2024-09-19 19:45:55 -0300

    Consertar a imagem no cadastro

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,11 @@ import { AppComponent } from './app.component';
 import { RouterModule } from '@angular/router';
 import { CadastroComponent } from './cadastro/cadastro.component';
 import { AppRoutingModule } from './app-routing.module';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [AppComponent, CadastroComponent],
-  imports: [RouterModule, BrowserModule, FormsModule, AppRoutingModule],
+  imports: [RouterModule, BrowserModule, FormsModule, AppRoutingModule, NgbModule],
   providers: [],
   bootstrap: [AppComponent],
 })
