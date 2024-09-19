2d8b5c29ad06f6d0279650c3690e6ebd36855718
Victor Hugo <victor.030farias@gmail.com>
2024-09-19 18:10:34 -0300

    Adicionar models de funcionário e autenticação

---
+++ src/app/shared/models/autenticacao.ts
@@ -0,0 +1,8 @@
+export class Autenticacao {
+  constructor(
+      public id?: number,
+      public login?: string,
+      public senha?: string,
+      public tipo?: string
+  ) {}
+}
---
+++ src/app/shared/models/funcionario.ts
@@ -0,0 +1,9 @@
+export class Funcionario {
+  constructor(
+      public id?: number,
+      public nome?: string,
+      public cpf?: string,
+      public email?: string,
+      public telefone?: string
+  ) {}
+}
