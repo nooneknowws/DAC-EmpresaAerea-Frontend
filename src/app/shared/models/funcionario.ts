3e9a03345cad21384e686c37aba23137e19fd159
fumireko <fumireko@yandex.com>
2024-09-24 18:35:05 -0300

    fixes

--- src/app/shared/models/funcionario.ts
+++ src/app/shared/models/funcionario.ts
@@ -2,7 +2,6 @@ import { Usuario } from "./usuario.model";
 
 export class Funcionario extends Usuario {
   constructor(
-    public cpf?: string,
     public telefone?: string,
     public matricula?: string
   ) { super(); }
