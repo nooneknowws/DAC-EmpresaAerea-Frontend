a120f544f1cdfd43ca962f1f62209e8887fd1096
fumireko <fumireko@yandex.com>
2024-10-02 09:31:34 -0300

    models de reserva e aeroporto, listagem na tela inicial

---
+++ src/app/shared/services/reserva.service.ts
@@ -0,0 +1,18 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Reserva } from '../models/reserva.model';
+import { Observable } from 'rxjs';
+import { AppComponent } from '../../app.component';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ReservaService {
+
+  private apiUrl: string = AppComponent.PUBLIC_BACKEND_URL;
+  constructor(private http: HttpClient) {}
+
+  getReservas(): Observable<Reserva[]> {
+    return this.http.get<Reserva[]>(`${this.apiUrl}/reservas`);
+  }
+}
