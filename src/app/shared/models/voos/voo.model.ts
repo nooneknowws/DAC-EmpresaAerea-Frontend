40965818f8bc0cd5402e3a932840e146937737ad
Thalyson <61944196+nooneknowws@users.noreply.github.com>
2024-10-02 19:17:21 -0300

    finalização do menu/cliente e implementação das funções de reservas em json-server

--- src/app/shared/models/voos/voo.model.ts
+++ src/app/shared/models/voos/voo.model.ts
@@ -2,6 +2,7 @@ import { Aeroporto } from './aeroporto.model';
 
 export class Voo {
     constructor(
+        public id?: string,
         public codigoVoo?: string,
         public dataHoraPartida?: Date,
         public origem?: Aeroporto,
