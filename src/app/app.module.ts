9e0566cdd4f7ac70a1844c05b30d60e3d1b8b46c
fumireko <fumireko@yandex.com>
2024-10-16 14:35:04 -0300

    Atualizar login e erros de import

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,18 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms'; // Import necessário para ngModel
-import { RouterModule } from '@angular/router'; // Import necessário para routerLink
-import { HttpClientModule } from '@angular/common/http';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-
 import { AppComponent } from './app.component';
-import { AppRoutingModule } from './app-routing.module';
-import { LoginComponent } from './autenticacao/login/login.component';
 import { CadastroComponent } from './autenticacao/cadastro/cadastro.component';
+import { LoginComponent } from './autenticacao/login/login.component';
 import { InicioComponent } from './inicio/inicio.component';
 import { ComprarComponent } from './milhas/comprar/comprar.component';
 import { ConsultarComponent } from './milhas/consultar/consultar.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { AppRoutingModule } from './app-routing.module';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { FuncionarioModule } from './funcionario/funcionario.module';
+import { HttpClientModule } from '@angular/common/http';
+
 
 @NgModule({
   declarations: [
--- src/app/cliente/cliente.module.ts
+++ src/app/cliente/cliente.module.ts
@@ -1,11 +1,11 @@
-import { CommonModule } from "@angular/common";
+import { NgModule } from "@angular/core";
+import { EfetuarReservaComponent } from "./reserva/efetuar-reserva/efetuar-reserva.component";
+import { DetalheReservaComponent } from "./reserva/detalhe-reserva/detalhe-reserva.component";
 import { CancelarReservaComponent } from "./reserva/cancelar-reserva/cancelar-reserva.component";
 import { ConsultarReservaComponent } from "./reserva/consultar-reserva/consultar-reserva.component";
-import { EfetuarReservaComponent } from "./reserva/efetuar-reserva/efetuar-reserva.component";
 import { FazerCheckinComponent } from "./reserva/fazer-checkin/fazer-checkin.component";
+import { CommonModule } from "@angular/common";
 import { ClienteRoutingModule } from "./cliente-routing.module";
-import { NgModule } from "@angular/core";
-import { DetalheReservaComponent } from "./reserva/detalhe-reserva/detalhe-reserva.component";
 
 @NgModule({
   declarations: [
