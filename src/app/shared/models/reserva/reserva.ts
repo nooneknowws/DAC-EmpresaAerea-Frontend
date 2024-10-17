7f97252e8bc2f907d3f497bb6c522cbb931dedcb
fumireko <fumireko@yandex.com>
2024-10-17 14:47:36 -0300

    Adicionar operador opcional nos models

--- src/app/shared/models/reserva/reserva.ts
+++ src/app/shared/models/reserva/reserva.ts
@@ -4,13 +4,13 @@ import { StatusReservaEnum } from "./status-reserva.enum";
 
 export class Reserva {
     constructor(
-        public id: number,
-        public dataHora: string,
-        public origem: Aeroporto,
-        public destino: Aeroporto,
-        public valor: number,
-        public milhas: number,
-        public status: StatusReservaEnum,
-        public voo: Voo
+        public id?: number,
+        public dataHora?: string,
+        public origem?: Aeroporto,
+        public destino?: Aeroporto,
+        public valor?: number,
+        public milhas?: number,
+        public status?: StatusReservaEnum,
+        public voo?: Voo
     ){}
 }
