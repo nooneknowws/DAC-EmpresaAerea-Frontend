9e4547fea28de5b68741e49f8604cc721354508c
Gabriel Chioquetta <gabriel.obladen@gmail.com>
2024-10-17 12:40:23 -0300

    wip: efetuar/consultar reservas
    criação das telas de efetuar e consultar reservas

--- src/app/cliente/reserva/efetuar-reserva/efetuar-reserva.component.ts
+++ src/app/cliente/reserva/efetuar-reserva/efetuar-reserva.component.ts
@@ -1,10 +1,10 @@
-import { Component } from '@angular/core';
+import { ReservaService } from './../../../shared/services/reserva.service';
+import { Component, OnInit } from '@angular/core';
+import { Reserva } from '../../../shared/models/reserva/reserva';
 
 @Component({
   selector: 'app-efetuar-reserva',
   templateUrl: './efetuar-reserva.component.html',
   styleUrl: './efetuar-reserva.component.css'
 })
-export class EfetuarReservaComponent {
-
-}
+export class EfetuarReservaComponent{}
