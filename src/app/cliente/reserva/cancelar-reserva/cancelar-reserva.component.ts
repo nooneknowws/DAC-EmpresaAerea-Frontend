100a80068cd97f92be2d615d4a77501ce26c8224
Victor Hugo <victor.030farias@gmail.com>
2024-10-20 17:39:38 -0300

    Adicionar informações à tela de cancelar voo

--- src/app/cliente/reserva/cancelar-reserva/cancelar-reserva.component.ts
+++ src/app/cliente/reserva/cancelar-reserva/cancelar-reserva.component.ts
@@ -1,4 +1,10 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ReservaService } from '../../../shared/services/reserva.service';
+import { Cliente } from '../../../shared/models/cliente/cliente';
+import { AuthService } from '../../../shared/services/auth.service';
+import { Reserva } from '../../../shared/models/reserva/reserva';
+import { StatusReservaEnum } from '../../../shared/models/reserva/status-reserva.enum';
 
 @Component({
   selector: 'app-cancelar-reserva',
@@ -6,5 +12,31 @@ import { Component } from '@angular/core';
   styleUrl: './cancelar-reserva.component.css'
 })
 export class CancelarReservaComponent {
+  user: Cliente | null = null;
+  reserva: Reserva | null = null;
+  loading: boolean = true;
+  errorMessage: string | null = null;
+  e = StatusReservaEnum;
+
+  constructor(
+    private reservaService: ReservaService,
+    private route: ActivatedRoute,
+    private authService: AuthService,
+  ) {}
+
+  ngOnInit(): void {
+    this.user = this.authService.getUser();
+    this.route.params.subscribe(params => {
+      const id = +params['id'];
+      this.getReserva(id);
+    });
+  }
+
+  getReserva(id: number): void {
+    this.reservaService.getReservaById(id).subscribe(reserva => {
+      this.reserva = reserva;
+      this.loading = false;
+    });
+  }
 
 }
